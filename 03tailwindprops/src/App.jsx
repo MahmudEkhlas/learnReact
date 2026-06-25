de529bdf2d189deae707659c2e1dd917306b9cc2
MahmudEkhlas <mahmudekhlas70@gmail.com>
2026-06-25 23:30:30 +0530

    counter

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Card from './components/card.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-red-500 bg-green-50 p-4'>tailwind test</h1>
+      <Card channel= "coffee"/>
+      <Card channel= "Cold Coffee"/>
+      
+    </>
+  )
+}
+
+export default App
